f6799d683cbe2b3e454e26302da951c59f45172f
Andrew Hess <ahess3815@gmail.com>
2018-09-20 14:08:15 -0500

    Reverted to the old loss functions

--- utils/losses.py
+++ utils/losses.py
@@ -10,8 +10,11 @@ def generator_loss(true, pred):
           human or by the generator.
     '''
 
-    # The loss in the original GAN paper is mean(log(1 - D(G(z)))).
-    return K.mean(K.log(K.ones(shape=tf.shape(pred)) - pred))
+    # The loss in the original GAN paper is mean(log(1 - D(G(z)))). However,
+    # this gives NaNs in the loss for some reason, so we use a different loss.
+    # return K.mean(K.log(K.ones(shape=tf.shape(pred)) - pred))
+
+    return K.mean(1 - K.abs(true - pred))
 
 
 def discriminator_loss(true, pred):
@@ -23,4 +26,7 @@ def discriminator_loss(true, pred):
     '''
 
     # The loss in the original GAN paper is mean(log(D(x)) + log(1 - D(G(z)))).
-    return K.mean(K.log(K.abs(true - pred)))
+    # However, this gives NaNs in the loss, so we use a different loss.
+    # return K.mean(K.log(K.abs(true - pred)))
+
+    return K.mean(K.abs(true - pred))
