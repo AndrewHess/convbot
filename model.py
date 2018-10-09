7fb5b3821f9566d27804be70304bd303c883b4b5
Andrew Hess <ahess3815@gmail.com>
2018-10-09 13:43:00 -0700

    Added relu activations to the models

--- model.py
+++ model.py
@@ -47,13 +47,13 @@ def build_model():
     ''' Build the generator, discriminator, and combined models. '''
 
     # Setup the memory.
-    random_input = Input(shape=(10,), name='rand_input')
+    random_input = Input(shape=(100,), name='rand_input')
 
     # Build the generator.
     gen_input = Input(shape=(input_size, vocab_len), name='gen_input')
-    encoded = Dense(10, name='gen_encoded')(gen_input)
+    encoded = Dense(10, activation='relu', name='gen_encoded')(gen_input)
     flatten = Flatten()(encoded)
-    gen_meaning = Dense(10, name='gen_meaning')(flatten)
+    gen_meaning = Dense(10, activation='relu', name='gen_meaning')(flatten)
     gen_mem_concat = Concatenate(name='gen_mem_concat')([gen_meaning, random_input])
 
     # Create probabilites for each word for each output location.
@@ -64,12 +64,12 @@ def build_model():
 
     # Build the discriminator.
     dis_input = Input(shape=(input_size, vocab_len), name='dis_input')
-    encoded_dis = Dense(10, name='dis_encoded')(dis_input)
-    encoded_gen = Dense(10, name='dis_gen_encoded')(gen_input)
+    encoded_dis = Dense(10, activation='relu', name='dis_encoded')(dis_input)
+    encoded_gen = Dense(10, activation='relu', name='dis_gen_encoded')(gen_input)
     flatten_dis = Flatten()(encoded_dis)
     flatten_gen = Flatten()(encoded_gen)
-    dis_meaning = Dense(10, name='dis_meaning')(flatten_dis)
-    dis_gen_meaning = Dense(10, name='dis_gen_meaning')(flatten_gen)
+    dis_meaning = Dense(10, activation='relu', name='dis_meaning')(flatten_dis)
+    dis_gen_meaning = Dense(10, activation='relu', name='dis_gen_meaning')(flatten_gen)
     dis_concat = Concatenate(name='dis_concat')([dis_gen_meaning, dis_meaning])
     dis_output = Dense(1, activation='sigmoid', name='dis_output')(dis_concat)
 
